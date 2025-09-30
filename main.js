e415dccd7a05aa21647a56dac8127e71d25ed4b1
sofiadelraz04-blip <sofiadelraz04@gmail.com>
2025-09-30 12:32:59 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function (event) {
+  if (event.target.hasAttribute("data-theme")) {
+    const themeFile = event.target.getAttribute("data-theme");
+    const themeButtons = document.querySelectorAll("[data-theme]");
+    themeButtons.forEach((btn) => btn.classList.remove("active-theme"));
+    event.target.classList.add("active-theme");
+    document.getElementById("themeStylesheet").setAttribute("href", themeFile);
+  }
+});
+
+clickHint.classList.toggle('hidden', controlesActivos);
+
+
